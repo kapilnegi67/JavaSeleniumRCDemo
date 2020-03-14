9a385fc1e2f7056b7dfecfc778c2f21772914cdc
kapilnegi67 <48856393+kapilnegi67@users.noreply.github.com>
2020-03-14 19:14:36 +0530

    Add files via upload

--- src/OpenGoogle.java
+++ src/OpenGoogle.java
@@ -2,7 +2,11 @@
  * 
  */
 //import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.thoughtworks.selenium.CommandProcessor;
 import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.webdriven.WebDriverCommandProcessor;
 
 /**
  * @author kapilnegi
@@ -14,11 +18,19 @@ public class OpenGoogle {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DefaultSelenium selenium = new DefaultSelenium("localhost", 4444, "*firefox /Applications/Firefox.app/Contents/MacOS/firefox", "https://www.google.com");
+		//DefaultSelenium selenium = new DefaultSelenium("localhost", 4444, "*firefox /Applications/Firefox.app/Contents/MacOS/firefox", "https://www.google.com");
+		System.setProperty("webdriver.chrome.driver", "/Users/kapilnegi/Desktop/chromedriver");
+		DefaultSelenium selenium = new DefaultSelenium((CommandProcessor) new WebDriverCommandProcessor("http://www.google.com",new ChromeDriver()));
 		selenium.start();
 		selenium.open("/");
 		selenium.windowMaximize();
 		selenium.type("q", "Learn Selenium");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		selenium.stop();
 
 	}
